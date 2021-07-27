c39843867101f9147e8dfb657ee876e1275f8af9
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-28 00:50:56 +0530

    [manish]:Added:create a contact

---
+++ AddressBook/AddressDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressDetails
+    {
+        string Firstname,Lastname, Address, City, State, Mail;
+        int Zip;
+        long Phonenumber;
+      
+        
+        public AddressDetails(string firstname,string lastname, string address, string state, string city, int zip, long phonenumber, string email)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Phonenumber = phonenumber;
+            this.Zip = zip;
+            this.Mail = email;
+        }
+        /// <summary>
+        /// Display Conctact details
+        /// </summary>
+        public void Contact()
+        {
+            Console.WriteLine("FirstName : "+this.Firstname+"\n" +"LastName : " + this.Lastname + "\n" + "Address : " + this.Address + "\n"
+                + "State : " + this.State+"\n"+ "City : " + this.City + "\n"+ "Zip : " + this.Zip + "\n"
+                +"PhoneNumber : " + this.Phonenumber + "\n" + "Mail Id : " + this.Mail);
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,9 +4,15 @@ namespace AddressBook
 {
     class Program
     {
+        /// <summary>
+        /// Displaying firstname,Lastname,Address,State,City,Pincode,Phonenumber,MailId
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            AddressDetails addressdetails = new AddressDetails("Manish","Pradhan","BNDM","Odisha","Rourkela",770032,7978353896, "manishyoyomp@gmail.com");
+            addressdetails.Contact();
         }
     }
 }
